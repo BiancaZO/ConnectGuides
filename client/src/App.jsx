ccd0a0697ead5d50b456243ad21a37091a3bd973
BiancaZO <89614618+BiancaZO@users.noreply.github.com>
2024-07-19 16:20:58 -0700

    First Commit-Configuration

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <header>
+      <h1>Connect Guides</h1>
+    </header>
+ 
+
+ </div>
+  )
+}
+
+export default App
