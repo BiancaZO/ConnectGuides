1a66b946f38f7030c9b6a0f7ada90e9b033dc991
Rafael Bastos <47896406+rmpbastos@users.noreply.github.com>
2024-07-20 14:43:16 -0700

    added login and register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
+import Layout from "./Layout";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
     </Routes>
-  )
+  );
 }
 
 export default App
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F5385D',
+      },
+    },
   },
   plugins: [],
 }
