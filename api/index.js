5d02db4f719017b8314cdb03d9d1693c72012b6b
Rafael Bastos <47896406+rmpbastos@users.noreply.github.com>
2024-07-20 22:37:26 -0700

    2nd part of login-register + account page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
 
@@ -14,6 +15,8 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'fasdgasdgqawegqadgas';
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use(cors({
     credentials: true,
@@ -48,9 +51,12 @@ app.post('/login', async (req, res) => {
   if (userDoc) {
     const passOk = bcrypt.compareSync(password, userDoc.password)
     if (passOk) {
-        jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err, token) => {
+        jwt.sign({
+            email:userDoc.email, 
+            id:userDoc._id
+        }, jwtSecret, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('token', token).json('pass ok');
+            res.cookie('token', token).json(userDoc);
         });    
     } else {
         res.status(422).json('pass not ok');
@@ -58,6 +64,23 @@ app.post('/login', async (req, res) => {
   } else {
     res.json('not found');
   }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name, email, _id} = await User.findById(userData.id);
+      res.json({name, email, _id});
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json(true);
 });
 
 app.listen(4000);
