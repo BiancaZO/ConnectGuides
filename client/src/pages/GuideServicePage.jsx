ae9b0a93c87dbf25a44817b7101aea2f71c3b068
Fabricio <briciolg13@gmail.com>
2024-07-20 22:46:03 -0700

    Guide service options2

--- client/src/pages/GuideServicePage.jsx
+++ client/src/pages/GuideServicePage.jsx
@@ -1,7 +1,12 @@
 import { useParams } from "react-router-dom";
+import Services from "../Services";
+import { useState } from "react";
 
 export default function GuiaServicePage(){
     const {action} = useParams();
+    const (title,setTitle) = useState{''};
+    const (city,setCity) = useState('');
+
     return(
         <div>
             {action !== 'new' && (
@@ -43,37 +48,7 @@ export default function GuiaServicePage(){
                         <textarea /> {/*ele colocou um textarea no index.css mas ja existe outro criado*/}
                         <h2 className="text-2xl mt-4">Services</h2>
                         <p  className="text-gray-500 text-sm">Select all services</p>
-                        <div className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-                            <label>
-                                <input type="checkbox"/>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                                </svg>
-                                <spam>night life</spam>
-                            </label>
-                            <label>
-                                <input type="checkbox"/>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                                </svg>
-                                <spam>day time</spam>
-                            </label>
-                            <label>
-                                <input type="checkbox"/>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                                </svg>
-                                <spam>adventure</spam>
-                            </label>
-                            <label>
-                                <input type="checkbox"/>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
-                                </svg>
-                                <spam>culture tour</spam>
-                            </label>
- 
-                        </div>
+                        <Services selected={services} onChange={setServices()}/>
 
                     </form>
                 </div>
