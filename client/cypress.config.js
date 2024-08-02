7862674cb8f4d0b1f543bd69ab55b4ce35a0d795
Rafael Bastos <47896406+rmpbastos@users.noreply.github.com>
2024-08-01 23:06:57 -0700

    added testing code using cypress

---
+++ client/cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173', // Adjust the port and URL as needed
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
